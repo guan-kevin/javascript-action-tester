98515a7f590e8510280b96ec5016bc7d63c6846d
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:12:15 -0700

    Create index.js

---
+++ subdirectory/index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const input1 = core.getInput('test_input-1');
+  const input2 = core.getInput('test_input-2');
+  const input3 = core.getInput('test_input-3');
+  const input4 = core.getInput('test_input-4');
+  
+  console.log("Running subdirectory")
+  console.log(`Input 1: ${input1}, Input 2: ${input2}, Input 3: ${input3}, Input 4: ${input4}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
