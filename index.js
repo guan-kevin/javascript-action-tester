18dccf4e54a1c4a7578252e670cdb09cf4957395
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-11-29 15:15:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const github = require('@actions/github');
 
 try {
   const input1 = core.getInput('test_input-1');
-  const input2 = core.getInput('test_input-2');
-  const input3 = core.getInput('test_input-3');
-  const input4 = core.getInput('test_input-4');
-  console.log(`Input 1: ${input1}, Input 2: ${input2}, Input 3: ${input3}, Input 4: ${input4}`);
-  console.log("Running commit sha")
+  console.log(`Input 1: ${input1}`);
+  console.log("Running old v4")
   
-  if (input1 != 1 || input2 != 2 || input3 != 3 || input4 != "") {
+  if (input1 != 4) {
     core.setFailed("Invalid input")
   }
 } catch (error) {
