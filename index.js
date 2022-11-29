7b9481729fb80b8fcc95a97748f3425ea6fe6109
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-11-29 15:21:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const github = require('@actions/github');
 try {
   const input1 = core.getInput('test_input-1');
   console.log(`Input 1: ${input1}`);
-  console.log("Running old v4")
-  
-  if (input1 != 4) {
-    core.setFailed("Invalid input")
-  }
+  console.log("Running new v4")
+  core.setFailed("Invalid input")
 } catch (error) {
   core.setFailed(error.message);
 }
